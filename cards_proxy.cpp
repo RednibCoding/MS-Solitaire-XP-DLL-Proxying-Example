a931731cb304dc80d42873e23bb14092460534b0
Michael Binder <rednib.coding@gmail.com>
2023-08-17 01:50:59 +0200

    added comments; updated readme

--- cards_proxy.cpp
+++ cards_proxy.cpp
@@ -1,48 +1,64 @@
-#include <iostream>
-#include <windows.h>
+// Include necessary headers
+#include <iostream>  // For input-output operations (used for printf here)
+#include <windows.h> // For Windows API functions
 
+// Define a macro for external DLL export, which tells the compiler that the function is intended to be exposed 
+// outside of the DLL, so other applications or DLLs can call it.
 #define EXTERN_DLL_EXPORT extern "C" __declspec(dllexport)
 
-// Ptr to the original cdtInit function
+// Define a function pointer type for the original cdtInit function
+// This pointer will be used to call the original function from the real cards.dll (now renamed cards_og.dll)
 typedef BOOL(WINAPI* cdtInitPtr)(int* width, int* height);
 
+// DllMain is a special function that's called when a DLL is loaded into or unloaded from memory.
 EXTERN_DLL_EXPORT BOOL APIENTRY DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call)
     {
-        switch (ul_reason_for_call)
-        {
-        case DLL_PROCESS_ATTACH:
-        case DLL_PROCESS_DETACH:
-        case DLL_THREAD_ATTACH:
-        case DLL_THREAD_DETACH:
-            break;
-        default:
-            break;
-        }
-        return TRUE;
+    case DLL_PROCESS_ATTACH: // Called when DLL is loaded into a process's memory space
+    case DLL_PROCESS_DETACH: // Called when DLL is unloaded from a process's memory space
+    case DLL_THREAD_ATTACH:  // Called when a thread is created within a process that has this DLL loaded
+    case DLL_THREAD_DETACH:  // Called when a thread is destroyed within a process that has this DLL loaded
+        break;
+    default: // Handle any other reasons, although generally not used
+        break;
     }
+    // Return TRUE to allow the DLL to load, otherwise returning FALSE would prevent it from loading
+    return TRUE;
 }
 
+// This is our modified version of the cdtInit function
 EXTERN_DLL_EXPORT BOOL cdtInit(int* width, int* height) {
+    // Load the original cards.dll (now renamed to cards_og.dll) into memory
     HINSTANCE cards_og_Dll = LoadLibrary(TEXT("cards_og.dll"));
+
+    // Check if the original DLL loaded successfully
     if (!cards_og_Dll) {
-        printf("Format messaged failed with 0x%x\n", GetLastError());
+        // Print an error message if failed to load, showing the error code
+        printf("Failed to load cards_og.dll with error code: 0x%x\n", GetLastError());
         return FALSE;
     }
 
+    // Get the address of the original cdtInit function from cards_og.dll
     cdtInitPtr cdtInit_og_function = (cdtInitPtr)GetProcAddress(cards_og_Dll, "cdtInit");
 
+    // Check if we successfully retrieved the address of the original function
     if (!cdtInit_og_function) {
-        printf("Format messaged failed with 0x%x\n", GetLastError());
+        // Print an error message if failed to retrieve, showing the error code
+        printf("Failed to find cdtInit function in cards_og.dll with error code: 0x%x\n", GetLastError());
         return FALSE;
     }
 
     /**** Start Weaponized Code ****/
+    // Any malicious or modified code you want to run before calling the original function goes here
 
     /**** End Weaponized code ****/
 
-    // Call the og function
+    // Call the original cdtInit function from cards_og.dll using our function pointer
     BOOL result = cdtInit_og_function(width, height);
+
+    // Free the loaded original cards_og.dll from memory. Important to release resources!
     FreeLibrary(cards_og_Dll);
 
+    // Return the result from the original function to the caller
     return result;
 }
--- readme.md
+++ readme.md
@@ -8,7 +8,29 @@ Specifically, this project targets the old game, MS Solitaire XP, and proxies th
 The proxy works by redirecting calls from the Solitaire game that are intended for the cards.dll to our proxy DLL (cards.dll).
 Once the calls are intercepted by our proxy, we can execute any additional code (for instance, any "weaponized" code), and then forward these calls to the original cards.dll (which we've renamed to cards_og.dll).
 
-## Building the Project
+## Function Forwarding with `cards.def`
+
+In our project, not all functions from the original `cards.dll` are intercepted for modifications. Many of them are simply forwarded to the original DLL without any intervention on our part.
+
+The `cards.def` file describes these functions. When the Solitaire game makes a call to any of these functions, our proxy DLL will transparently forward the call to the original `cards.dll` (renamed to `cards_og.dll`). This ensures that only specific functions (those we choose) are intercepted and modified, while the rest maintain their original behavior.
+
+Here's a snippet from the `cards.def` file:
+
+```
+EXPORTS
+cdtAnimate=cards_og.dll.cdtAnimate @2
+cdtDraw=cards_og.dll.cdtDraw @3
+cdtDrawExt=cards_og.dll.cdtDrawExt @4
+cdtTerm=cards_og.dll.cdtTerm @6
+```
+
+For functions where we intend to introduce our own behavior or code, we need to provide their definitions in the `cards_proxy.cpp` file. This allows us to modify, intercept, or enhance their behavior as needed.
+
+## Dive Deeper with Code Comments
+
+For those interested in a more in-depth understanding of the DLL proxying mechanism, refer directly to the `cards_proxy.cpp` file. I've included detailed comments for each significant code segment, ensuring that you grasp the nuances of each operation. Whether you're a beginner or an expert, the code annotations will guide you through the intricacies of DLL proxying in this project.
+
+## Building the Project with MSYS2
 
 1. Compile the DLL Proxy:
 
@@ -27,7 +49,7 @@ Once the calls are intercepted by our proxy, we can execute any additional code 
 3. Run MS Solitaire XP:
    - Launch the game. It will now use the proxy DLL, which intercepts and can modify the behavior of certain functions.
 
-# Setting up MSYS2 on Windows (only for Windows users)
+# Setting up MSYS2 on Windows (only for Windows users that don't have MSYS2 installed)
 
 To compile the project on Windows, we recommend using MSYS2. Here's how to set it up:
 
